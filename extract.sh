e9b912b18197d967f7fe29622f9780b35dab25bd
Victor Hugo Candido de Oliveira <victorhcandido@gmail.com>
2019-06-06 00:24:04 -0300

    Removing this comment

--- extract.sh
+++ extract.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-y=2014
+y=2012
 for m in {01..12};
 do
 	find . -path "./data/zip/$y/$m*zip" -exec sh -c 'dir="${1#*zip/}"; unzip -d "data/tick/${dir%/*}" "$1"' _ {} \;
 done
-#find . -path "./data/zip/2013/01*zip" -exec unzip {} -d data/tick/2013/01\ January/ \;
